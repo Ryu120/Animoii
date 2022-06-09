98b32f4c063da34a6803368d2c3832c3b3ddfd29
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-06-09 09:51:04 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
 from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
 from pyrogram import Client, filters
+import tracemoepy
 from requests import get
 import time , datetime
 import os
 import re
+
 
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
@@ -81,6 +83,19 @@ def callbackk(_,query):
         except:
             query.answer("Refreshed!")
 
+       tracemoe = tracemoepy.tracemoe.TraceMoe()
+
+@bot.on_message(filters.command('whatanime'))
+def whatanime(_,message):
+  reply = message.reply_to_message
+  if reply and reply.media:
+    path = reply.download()
+    info = tracemoe.search(path, upload_file=True)
+    data = f"Match: {info.result[0].anilist.title.romaji}\nSimilarity: {info.result[0].similarity*100}"
+    info.result[0].save(f"{reply.from_user.id}.mp4", mute = False)
+    reply.reply_document(f"{reply.from_user.id}.mp4" , caption=data)
+    os.remove(f"{reply.from_user.id}.mp4")     
+            
 
 
 
