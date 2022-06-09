a1ed9d956b671d92c0598430ccc6bdfe2cc17168
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-06-09 09:47:52 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,88 @@
+from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
+from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
+from pyrogram import Client, filters
+from requests import get
+import time , datetime
+import os
+import re
+
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+
+
+bot = Client(
+    "Kawaii",
+    api_id = API_ID, 
+    api_hash = API_HASH, 
+    bot_token = BOT_TOKEN)
+
+def call_back_in_filter(data):
+    return filters.create(
+        lambda flt, _, query: flt.data in query.data,
+        data=data
+    )
+
+def latest():
+
+    url = 'https://subsplease.org/api/?f=schedule&h=true&tz=Japan'
+    res = get(url).json()
+
+    k = None 
+    for x in res['schedule']:
+        title = x['title']
+        time = x['time']
+        aired = bool(x['aired'])
+        title = f"**[{title}](https://subsplease.org/shows/{x['page']})**" if not aired else f"**~~[{title}](https://subsplease.org/shows/{x['page']})~~**"
+        data = f"{title} - {time}"
+
+        if k:
+            k = f"{k}\n{data}"
+
+        else:
+            k = data
+
+
+    return k
+
+
+
+@bot.on_message(filters.command('latest'))
+def lates(_,message):
+    mm = latest()
+    message.reply_text(f"Today's Schedule:\nTZ: Japan\n{mm}" , reply_markup=InlineKeyboardMarkup(
+    [    
+        [InlineKeyboardButton("Refresh" , callback_data="animoii")]
+
+    ]
+        
+    ))
+
+
+@bot.on_callback_query(call_back_in_filter("animoii"))
+def callbackk(_,query):
+
+    if query.data == "animoii":
+        mm = latest()
+        time_ = datetime.datetime.now(datetime.timezone.utc).strftime("%H:%M")
+
+
+        try:
+            query.message.edit(f"Today\'s Schedule:\nTZ: Japan\n{mm}", reply_markup=InlineKeyboardMarkup(
+        [    
+            [InlineKeyboardButton("Refresh" , callback_data="animoii")]
+
+        ]
+            
+        ))
+            query.answer("Refreshed!")
+
+
+        except:
+            query.answer("Refreshed!")
+
+
+
+
+if __name__ == "__main__":
+    bot.run()
